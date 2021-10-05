4bbc3aa564c257cabef234e6cb48a2aa2d673224
Umer Hurramov <umer.hurramov@gmail.com>
2021-10-05 20:56:56 +0300

    markup for home screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,136 +4,165 @@ void main() {
   runApp(const MyApp());
 }
 
+var appColors = const {
+  'primary': Color.fromARGB(255, 0, 140, 14),
+  'totalCellBg': Color.fromARGB(105, 0, 0, 0),
+  'evenCellBg': Color.fromARGB(158, 255, 255, 255),
+  'oddCellBg': Color.fromARGB(209, 255, 255, 255),
+  'buttonBg': Colors.white
+};
+
+class Cell extends StatelessWidget {
+  const Cell(
+      {Key? key, required this.text, this.isTotal = false, this.isEven = false})
+      : super(key: key);
+  final bool isEven;
+  final bool isTotal;
+  final String text;
+
+  _getTextColor() {
+    return isTotal ? Colors.white : Colors.black;
+  }
+
+  _getBackgroundColor() {
+    return isTotal
+        ? appColors['totalCellBg']
+        : isEven
+            ? appColors['evenCellBg']
+            : appColors['oddCellBg'];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 10),
+          margin: const EdgeInsets.fromLTRB(0, 0, 1, 1),
+          decoration: BoxDecoration(color: _getBackgroundColor()),
+          child: Center(
+              child: Text(text,
+                  style: TextStyle(
+                      color: _getTextColor(),
+                      fontSize: 16,
+                      fontFamily: 'Roboto')))),
+    );
+  }
+}
+
+class Record extends StatelessWidget {
+  const Record(
+      {Key? key,
+      required this.number,
+      required this.usValue,
+      required this.theyValue,
+      this.isTotal = false,
+      this.isEven = false})
+      : super(key: key);
+
+  final bool isEven;
+  final bool isTotal;
+  final String number;
+  final String usValue;
+  final String theyValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Cell(text: number, isTotal: isTotal, isEven: isEven),
+        Cell(text: usValue, isTotal: isTotal, isEven: isEven),
+        Cell(text: theyValue, isTotal: isTotal, isEven: isEven),
+      ],
+    );
+  }
+}
+
+class TopContent extends StatelessWidget {
+  const TopContent({Key? key}) : super(key: key);
+
+  _getRecords() {
+    var records = <Record>[];
+    for (var i = 1; i <= 20; i++) {
+      var r = Record(
+        number: i.toString(),
+        usValue: '100',
+        theyValue: '82',
+        isEven: i % 2 == 0,
+      );
+      records.add(r);
+    }
+    return records;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView(
+        padding: const EdgeInsets.fromLTRB(20, 40, 20, 20),
+        shrinkWrap: true,
+        children: [
+          const Record(
+              number: '#', usValue: 'US', theyValue: 'THEY', isTotal: true),
+          ..._getRecords(),
+          const Record(
+              number: 'TOTAL', usValue: '200', theyValue: '164', isTotal: true),
+        ],
+      ),
+    );
+  }
+}
+
+class BottomContent extends StatelessWidget {
+  const BottomContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // decoration: const BoxDecoration(color: Colors.red),
+      height: 100,
+      padding: const EdgeInsets.fromLTRB(20, 20, 20, 30),
+      child: Expanded(
+          child: OutlinedButton(
+              onPressed: () {
+                print(1);
+              },
+              child: Text('Enter points'.toUpperCase(),
+                  style: const TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w600)),
+              style: OutlinedButton.styleFrom(
+                  primary: appColors['primary'],
+                  backgroundColor: appColors['buttonBg']))),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(
+              color: appColors['primary'],
+              image: const DecorationImage(
+                  image: AssetImage('assets/images/cards.png'))),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [TopContent(), BottomContent()])),
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have 6 the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Table(
-                // textDirection: TextDirection.rtl,
-                // defaultVerticalAlignment: TableCellVerticalAlignment.bottom,
-                // border:TableBorder.all(width: 2.0,color: Colors.red),
-                children: const [
-                  TableRow(
-                    children: [
-                      Text("Game #",textScaleFactor: 1.5,),
-                      Text("US",textScaleFactor: 1.5),
-                      Text("THEY",textScaleFactor: 1.5),
-                    ]
-                  ),
-                  TableRow(
-                    children: [
-                      Text("Total",textScaleFactor: 1.5),
-                      Text("total sum",textScaleFactor: 1.5),
-                      Text("total sum",textScaleFactor: 1.5),
-                    ]
-                  ),
-                ],
-              ),
-              ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return const MaterialApp(title: 'Debertz Record', home: Home());
   }
 }
